e8dfb2550994d72a7e2ee72f25ee1ac200a6fd4e
Fabian <fabianjoseph063@gmail.com>
2026-01-31 04:47:28 +0100

    Update log messages in on_click function

--- src/main.py
+++ src/main.py
@@ -4,12 +4,12 @@ import flet as ft
 def on_click(e):
     from jnius import autoclass
     Log = autoclass("android.util.Log")  # TODO Replace when on android
-    Log.e("test","world")
+    Log.e("python","found java logger")
 
     from android_notify import Notification
     Notification(title="Hello Earth").send()
 
-    Log.e("test","world1")
+    Log.e("test","found android notify")
 
 def main(page: ft.Page):
     page.title = "Logging Example"

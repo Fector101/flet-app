12dd26dbff117c726f63b8735bd6d7c4bf091832
Fabian <fabianjoseph063@gmail.com>
2026-08-16 20:32:04 +0100

    android notify has a patch for auto ids

--- src/tests/test_android_notify_full.py
+++ src/tests/test_android_notify_full.py
@@ -12,20 +12,16 @@ class TestAndroidNotifyFull(unittest.TestCase):
     def tearDown(self):
         time.sleep(3)
 
-    def setUp(self):
-        max_int = 2_147_483_647
-        self.uid = int(time.time() * 1000) % max_int
-
     def test_simple(self):
         try:
-            n = Notification( id=self.uid,title="Simple", message="Simple message test")
+            n = Notification(title="Simple", message="Simple message test")
             n.send()
         except Exception as e:
             self.fail(f"Simple notification failed: {e}")
 
     def test_progress(self):
         try:
-            n = Notification( id=self.uid,title="Downloading...", message="0% downloaded", progress_current_value=0, progress_max_value=100)
+            n = Notification(title="Downloading...", message="0% downloaded", progress_current_value=0, progress_max_value=100)
             n.send()
             for i in range(0, 101, 10):
                 time.sleep(2)
@@ -36,7 +32,7 @@ class TestAndroidNotifyFull(unittest.TestCase):
 
     def test_big_picture(self):
         try:
-            n = Notification( id=self.uid,title="Big Picture", message="Testing big picture")
+            n = Notification(title="Big Picture", message="Testing big picture")
             n.setBigPicture("assets/icon.png")
             n.send()
         except Exception as e:
@@ -44,7 +40,7 @@ class TestAndroidNotifyFull(unittest.TestCase):
 
     def test_inbox(self):
         try:
-            n = Notification( id=self.uid,title="Inbox", message="Inbox notification")
+            n = Notification(title="Inbox", message="Inbox notification")
             n.setLines(["Line 1", "Line 2", "Line 3"])
             n.send()
         except Exception as e:
@@ -53,14 +49,14 @@ class TestAndroidNotifyFull(unittest.TestCase):
     def test_inbox_from_file_message(self):
         try:
             lines = "Test1\nTest2\nTest3"
-            n = Notification( id=self.uid,title="Inbox File", message="File Inbox Test", lines_txt=lines)
+            n = Notification(title="Inbox File", message="File Inbox Test", lines_txt=lines)
             n.send()
         except Exception as e:
             self.fail(f"Inbox from file failed: {e}")
 
     def test_inbox_add_line(self):
         try:
-            n = Notification( id=self.uid,title="Inbox AddLine", message="Testing addLine()")
+            n = Notification(title="Inbox AddLine", message="Testing addLine()")
             n.addLine("First line")
             n.addLine("Second line")
             n.addLine("Third line")
@@ -70,7 +66,7 @@ class TestAndroidNotifyFull(unittest.TestCase):
 
     def test_large_icon(self):
         try:
-            n = Notification( id=self.uid,title="Large Icon", message="Testing large icon")
+            n = Notification(title="Large Icon", message="Testing large icon")
             n.setLargeIcon("assets/icon.png")
             n.send()
         except Exception as e:
@@ -78,7 +74,7 @@ class TestAndroidNotifyFull(unittest.TestCase):
 
     def test_big_text(self):
         try:
-            n = Notification( id=self.uid,title="Big Text", message="Testing big text")
+            n = Notification(title="Big Text", message="Testing big text")
             n.setBigText("Lorem Ipsum is dummy text for testing BigTextStyle display.")
             n.send()
         except Exception as e:
@@ -86,7 +82,7 @@ class TestAndroidNotifyFull(unittest.TestCase):
 
     def test_buttons(self):
         try:
-            n = Notification( id=self.uid,title="With Buttons", message="Testing action buttons")
+            n = Notification(title="With Buttons", message="Testing action buttons")
             n.addButton(text="Play", on_release=lambda: print("Playing"))
             n.addButton(text="Pause", on_release=lambda: print("Paused"))
             n.addButton(text="Stop", on_release=lambda: print("Stopped"))
@@ -96,7 +92,7 @@ class TestAndroidNotifyFull(unittest.TestCase):
 
     def test_both_imgs(self):
         try:
-            n = Notification( id=self.uid,title="Both Images", message="Testing both large & big picture")
+            n = Notification(title="Both Images", message="Testing both large & big picture")
             n.setLargeIcon("assets/icon.png")
             n.setBigPicture("assets/icon.png")
             n.send()
@@ -105,7 +101,7 @@ class TestAndroidNotifyFull(unittest.TestCase):
 
     def test_custom_icon(self):
         try:
-            n = Notification( id=self.uid,title="Custom Icon", message="Testing custom app icon")
+            n = Notification(title="Custom Icon", message="Testing custom app icon")
             n.setSmallIcon("assets/icon.png")
             n.send()
         except Exception as e:
@@ -113,7 +109,7 @@ class TestAndroidNotifyFull(unittest.TestCase):
 
     def test_title_message_update(self):
         try:
-            n = Notification( id=self.uid,title="Old Title", message="Old Message")
+            n = Notification(title="Old Title", message="Old Message")
             n.send()
             time.sleep(2)
             n.updateTitle("New Title")
@@ -123,7 +119,7 @@ class TestAndroidNotifyFull(unittest.TestCase):
 
     def test_download_channel(self):
         try:
-            n = Notification( id=self.uid,
+            n = Notification(
                 title="Download Done",
                 message="Your file finished downloading.",
                 channel_name="Download Notifications",
@@ -135,7 +131,7 @@ class TestAndroidNotifyFull(unittest.TestCase):
 
     def test_channel_generate_id(self):
         try:
-            n = Notification( id=self.uid,
+            n = Notification(
                 title="Generated Channel",
                 message="Channel creation test",
                 channel_name="Custom Channel"
@@ -146,21 +142,21 @@ class TestAndroidNotifyFull(unittest.TestCase):
 
     def test_custom_id(self):
         try:
-            n = Notification( id=self.uid,title="Custom ID", message="Click to trigger handler", name="change_app_page")
+            n = Notification(title="Custom ID", message="Click to trigger handler", name="change_app_page")
             n.send()
         except Exception as e:
             self.fail(f"Custom ID failed: {e}")
 
     def test_callback(self):
         try:
-            n = Notification( id=self.uid,title="With Callback", message="Tap to run callback", callback=lambda: print("Callback invoked"))
+            n = Notification(title="With Callback", message="Tap to run callback", callback=lambda: print("Callback invoked"))
             n.send()
         except Exception as e:
             self.fail(f"Callback failed: {e}")
 
     def test_custom_channel_name(self):
         try:
-            n = Notification( id=self.uid,title="Custom Channel", message="Testing custom name", channel_name="MyChannelName")
+            n = Notification(title="Custom Channel", message="Testing custom name", channel_name="MyChannelName")
             n.send()
         except Exception as e:
             self.fail(f"Custom channel name failed: {e}")
@@ -173,7 +169,7 @@ class TestAndroidNotifyFull(unittest.TestCase):
 
     def test_create_channel_lifespan(self):
         try:
-            n = Notification( id=self.uid,title="Check ID Lifespan", message="Testing channel id reuse")
+            n = Notification(title="Check ID Lifespan", message="Testing channel id reuse")
             cid = "test_channel_id"
             n.createChannel(id=cid, name="Test Channel", description="Lifespan check")
             n.send()
@@ -182,14 +178,14 @@ class TestAndroidNotifyFull(unittest.TestCase):
 
     def test_persistent(self):
         try:
-            n = Notification( id=self.uid,title="Persistent", message="This notification should persist")
+            n = Notification(title="Persistent", message="This notification should persist")
             n.send(persistent=True)
         except Exception as e:
             self.fail(f"Persistent notification failed: {e}")
 
     # def test_get_active_notifications(self):
     #     try:
-    #         Notification( id=self.uid,).get_active_notifications()
+    #         Notification().get_active_notifications()
     #     except Exception as e:
     #         self.fail(f"Get active notifications failed: {e}")
 
